6f6a169cf60a52cb87743ee8b38766ad103264cb
Mcusac <matthew.d.cusack@gmail.com>
2022-06-16 22:41:36 -0500

    thursfin

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, redirect
-import pymongo as PyMongo
+from flask_pymongo import PyMongo
 import scrape_to_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
